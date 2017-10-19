fac2472ca8c9c3e154952449dfcb41a15d7aaa2c
richard lacey <rlacey@wit.ie>
2017-10-19 16:07:00 +0100

    tdd04 03_a

--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -4,20 +4,12 @@ import static org.junit.Assert.*;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+
+import static models.Fixtures.users;
 
 public class UserTest
 {
-  private User[] users =
-  {
-    new User ("marge", "simpson", "marge@simpson.com",  "secret"),
-    new User ("lisa",  "simpson", "lisa@simpson.com",   "secret"),
-    new User ("bart",  "simpson", "bart@simpson.com",   "secret"),
-    new User ("maggie","simpson", "maggie@simpson.com", "secret")
-  };
   User homer = new User ("homer", "simpson", "homer@simpson.com",  "secret");
 
   @Test
@@ -43,6 +35,7 @@ public class UserTest
   @Test
   public void testToString()
   {
+	System.out.println(homer.toString());
     assertEquals ("User{" + homer.id + ", homer, simpson, secret, homer@simpson.com}", homer.toString());
   }
 }
